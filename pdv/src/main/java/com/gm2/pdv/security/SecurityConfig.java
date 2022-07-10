08e3b869c5e6da4d310ee6d0bb6f9304bdd7d106
Genilson Medeiros Martins <genilson.martins@virtus.ufcg.edu.br>
2022-07-10 10:56:10 -0300

    frontend vue para teste

--- pdv/src/main/java/com/gm2/pdv/security/SecurityConfig.java
+++ pdv/src/main/java/com/gm2/pdv/security/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
-        http.csrf().disable()
+        http.cors().and().csrf().disable()
                 .authorizeRequests()
                     .antMatchers("/info").permitAll()
                 .anyRequest().authenticated()
---
+++ pdv/src/main/java/com/gm2/pdv/security/WebConfiguration.java
@@ -0,0 +1,22 @@
+package com.gm2.pdv.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@EnableWebMvc
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(false)
+                .allowedMethods("GET");
+    }
+}
